b5e7e3e9ac099cc8fd612d7409f045b0e2d24b3d
André Birk <arbi@itu.dk>
2026-03-23 17:01:11 +0100

    92% on algorithms in jacoco

--- lib/src/main/java/io/github/mcalgovisualizations/visualization/algorithms/AlgorithmStepper.java
+++ lib/src/main/java/io/github/mcalgovisualizations/visualization/algorithms/AlgorithmStepper.java
@@ -5,6 +5,7 @@ import io.github.mcalgovisualizations.visualization.algorithms.events.IAlgorithm
 import io.github.mcalgovisualizations.visualization.algorithms.events.NoOp;
 import io.github.mcalgovisualizations.visualization.models.Data;
 import io.github.mcalgovisualizations.visualization.models.SortingCollection;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -12,14 +13,14 @@ import java.util.List;
 import java.util.Random;
 
 public class AlgorithmStepper<T extends Comparable<T>> {
-    private final Random RANDOM = new Random(64);
+    private final Random RANDOM = new Random();
     private final ArrayList<IAlgorithmEvent> history = new ArrayList<>();
     private SortingCollection<T> collection; // TODO : Make an interface for this
     private final IPlayerSort algorithm;
 
     private int historyPointer = 0;
 
-    public AlgorithmStepper(IPlayerSort algorithm, SortingCollection<T> collection) {
+    public AlgorithmStepper(@NotNull IPlayerSort algorithm, @NotNull SortingCollection<T> collection) {
         this.algorithm = algorithm;
         this.collection = collection;
     }
@@ -30,16 +31,7 @@ public class AlgorithmStepper<T extends Comparable<T>> {
      */
     public List<Data<T>> getBackingCollection() {
         var out = List.copyOf(collection.data());
-
-        history.clear();
-        historyPointer = 0;
-
-        algorithm.sort(collection);
-        var parsedEvents = new ArrayList<>(collection.events());
-        parsedEvents.add(new Complete(collection.size()));
-
-        this.history.addAll(parsedEvents);
-
+        rebuildHistory();
         return out;
     }
 
@@ -61,26 +53,26 @@ public class AlgorithmStepper<T extends Comparable<T>> {
         if(history.isEmpty()) return new NoOp();
         // check if we are already at the beginning of the history
         if (historyPointer <= 0) return new NoOp();
-        return history.get(historyPointer--);
+        return history.get(--historyPointer);
     }
 
     public List<Data<T>> randomizeCollection(int seed) {
-        RANDOM.setSeed(seed);
-        // create new collection with randomized data
         var data = new ArrayList<>(collection.data());
-        Collections.shuffle(data, RANDOM);
+        Collections.shuffle(data, new Random(seed));
+
         this.collection.clear();
         this.collection = new SortingCollection<>(data);
 
-        // clear history and start over
-        this.history.clear();
-        this.historyPointer = 0;
-        this.algorithm.sort(collection);
-        var parsedEvents = new ArrayList<>(collection.events());
-        parsedEvents.add(new Complete(collection.size()));
-        this.history.addAll(parsedEvents);
-
+        rebuildHistory();
         return List.copyOf(data);
+    }
+
+    private void rebuildHistory() {
+        history.clear();
+        historyPointer = 0;
+        algorithm.sort(collection);
+        history.addAll(collection.events());
+        history.add(new Complete(collection.size()));
     }
 
     public String getAlgoName() {
