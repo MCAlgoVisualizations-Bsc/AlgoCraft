11cf2cfe033c5e13c9f24cd6d88e4be792ee638f
Lukas <lukasvranic@gmail.com>
2026-05-11 16:25:13 +0200

    added randomize back

--- server/src/main/java/io/github/mcalgovisualizations/algorithms/GraphSearch/NodeUtils.java
+++ server/src/main/java/io/github/mcalgovisualizations/algorithms/GraphSearch/NodeUtils.java
@@ -1,11 +1,13 @@
 package io.github.mcalgovisualizations.algorithms.GraphSearch;
 
+import java.util.*;
+
 public class NodeUtils {
     public static final float GRID_SCALE = .75f;
     public static final int GRID_ROWS = (int) (4 * GRID_SCALE);
     public static final int GRID_COLS = (int) (6 * GRID_SCALE);
 
-    public static Node RandomizeNode() {
+    public static Node test() {
         Node one = new Node(0, 1, Node.NodeTarget.Start);
         Node two = new Node(1, 2, Node.NodeTarget.None);
         Node three = new Node(2, 3, Node.NodeTarget.None);
@@ -27,6 +29,58 @@ public class NodeUtils {
 
 
         return one;
+    }
+
+    public static Node RandomizeNode() {
+        Random random = new Random();
+        Map<String, Node> grid = new HashMap<>();
+        List<Node> existingNodes = new ArrayList<>();
+
+        // Starting point
+        Node startNode = new Node(0, 0, Node.NodeTarget.Start);
+        grid.put("0:0", startNode);
+        existingNodes.add(startNode);
+
+        int[][] directions = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+
+        for (int i = 1; i < 8; i++) {
+            boolean placed = false;
+            while (!placed) {
+                // Pick a random node we've already placed to branch off from
+                Node parent = existingNodes.get(random.nextInt(existingNodes.size()));
+
+                // This is a bit of a hack: we'd need to store coordinates in the Node
+                // or a wrapper to know where the 'parent' is.
+                // Let's assume we find its coords from the map:
+                String parentKey = grid.entrySet().stream()
+                        .filter(e -> e.getValue().equals(parent))
+                        .map(java.util.Map.Entry::getKey)
+                        .findFirst().get();
+
+                String[] parts = parentKey.split(":");
+                int nextX = Integer.parseInt(parts[0]);
+                int nextZ = Integer.parseInt(parts[1]);
+
+                int[] dir = directions[random.nextInt(4)];
+                nextX += dir[0];
+                nextZ += dir[1];
+
+                String newKey = nextX + ":" + nextZ;
+
+                if (!grid.containsKey(newKey)) {
+                    Node newNode = new Node(i, i, Node.NodeTarget.None);
+                    grid.put(newKey, newNode);
+                    existingNodes.add(newNode);
+                    connectUndirected(parent, newNode);
+                    placed = true;
+                }
+            }
+        }
+
+        // Set a random node (other than start) as the end
+        existingNodes.getLast().setStatus(Node.NodeTarget.End);
+
+        return startNode;
     }
 
     private static void connectUndirected(Node a, Node b) {
