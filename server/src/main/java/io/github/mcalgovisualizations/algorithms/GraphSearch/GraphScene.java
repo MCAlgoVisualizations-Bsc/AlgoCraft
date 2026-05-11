2d2ff582d2644e225e69b29f6e14a76fb966a9ed
Lukas <lukasvranic@gmail.com>
2026-05-11 17:03:49 +0200

    added villagerPov back

--- server/src/main/java/io/github/mcalgovisualizations/algorithms/GraphSearch/GraphScene.java
+++ server/src/main/java/io/github/mcalgovisualizations/algorithms/GraphSearch/GraphScene.java
@@ -1,8 +1,11 @@
 package io.github.mcalgovisualizations.algorithms.GraphSearch;
 
+import io.github.mcalgovisualizations.Displays.EntityCreatureDisplay;
 import io.github.mcalgovisualizations.visualization.renderer.LayoutResult;
 import io.github.mcalgovisualizations.visualization.renderer.scene.AbstractScene;
 import io.github.mcalgovisualizations.visualization.renderer.scene.SceneContext;
+import io.github.mcalgovisualizations.visualization.renderer.scene.VillagerPOV;
+import net.minestom.server.entity.Entity;
 import net.minestom.server.entity.ItemEntity;
 import net.minestom.server.instance.Instance;
 import org.jetbrains.annotations.NotNull;
@@ -10,7 +13,8 @@ import org.jetbrains.annotations.NotNull;
 import java.util.ArrayList;
 import java.util.List;
 
-public class GraphScene extends AbstractScene {
+public class GraphScene extends AbstractScene implements VillagerPOV {
+    private static final int SEARCHER_SLOT = -100;
     private final List<ItemEntity> extraEntities = new ArrayList<>();
 
     public GraphScene(@NotNull SceneContext context) {
@@ -23,6 +27,14 @@ public class GraphScene extends AbstractScene {
 
     public void addItemEntity(ItemEntity entity) {
         extraEntities.add(entity);
+    }
+
+    public Entity cameraTarget() {
+        var display = getDisplay(SEARCHER_SLOT);
+        if (display instanceof EntityCreatureDisplay creatureDisplay) {
+            return creatureDisplay.getEntity();
+        }
+        return null;
     }
 
     @Override
