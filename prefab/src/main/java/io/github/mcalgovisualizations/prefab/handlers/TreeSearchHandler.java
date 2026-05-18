8b5a3a080b75e9e774776b9cddebfa59b0dd43bb
Lukas <lukasvranic@gmail.com>
2026-05-18 14:54:22 +0200

    removed some stuff

--- prefab/src/main/java/io/github/mcalgovisualizations/prefab/handlers/TreeSearchHandler.java
+++
@@ -1,7 +0,0 @@
-package io.github.mcalgovisualizations.prefab.handlers;
-
-/**
- * @deprecated Logic moved to BstCompareHandler to support all tree searches.
- */
-@Deprecated
-public final class TreeSearchHandler {}
